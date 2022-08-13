4a509c3f2be663c20f844ae8bcf10c0ea9e4a652
Athanasius <github@miggy.org>
2022-08-13 14:30:08 +0100

    load.py: Rest of flake8 pass

--- load.py
+++ load.py
@@ -7,31 +7,25 @@ import sys
 import time
 import tkinter as tk
 
+from config import config
 from l10n import Locale
 
 this = sys.modules[__name__]  # For holding module globals
-
-try:
-    from config import config
-except ImportError:
-    config = dict()
 
 CFG_EARNINGS = "EarningSpeed_earnings"
 
 
 class Transaction(object):
-    """
-    Represents a transaction
-    """
+    """The details about a single transaction."""
+
     earnings = 0.0
     time = 0
     is_docking_event = 0.0
 
 
 class HourlyIncome(object):
-    """
-    The main class for the hourlyincome plugin
-    """
+    """The main class for the hourlyincome plugin."""
+
     speed_widget = None
     rate_widget = None
     earned_widget = None
@@ -39,20 +33,14 @@ class HourlyIncome(object):
     transactions = []
 
     def reset(self):
-        """
-        Reset button pressed
-        :return:
-        """
+        """Handle the reset button being pressed."""
         self.transactions = []
         self.saved_earnings = 0
         self.update_window()
         self.save()
 
     def load(self):
-        """
-        Load saved earnings from config
-        :return:
-        """
+        """Load saved earnings from config."""
         saved = config.get_str(CFG_EARNINGS)
         if not saved:
             self.saved_earnings = 0.0
@@ -60,17 +48,14 @@ class HourlyIncome(object):
             self.saved_earnings = float(saved)
 
     def save(self):
-        """
-        Save the saved earnings to config
-        :return:
-        """
+        """Save the saved earnings to config."""
         config.set(CFG_EARNINGS, str(self.saved_earnings + self.trip_earnings()))
 
     def transaction(self, earnings):
         """
-        Record a transaction
+        Record a transaction.
+
         :param earnings:
-        :return:
         """
         data = Transaction()
         data.earnings = earnings
@@ -81,11 +66,7 @@ class HourlyIncome(object):
         self.save()
 
     def register_docking(self):
-        """
-        Record a transaction
-        :param earnings:
-        :return:
-        """
+        """Record a docking event."""
         data = Transaction()
         data.earnings = 0.0
         data.is_docking_event = 1.0
@@ -96,15 +77,17 @@ class HourlyIncome(object):
 
     def trip_earnings(self):
         """
-        Measure how much we've earned
-        :return:
+        Calculate the current trip earnings.
+
+        :return: int - the current trip earnings
         """
         return sum([x.earnings for x in self.transactions])
 
     def rate(self):
         """
-        Get the station visits/hr rate
-        :return:
+        Calculate the rate of station visits/hour.
+
+        :return: Calculated visits/hour
         """
         if len(self.transactions) > 1:
             started = self.transactions[0].time
@@ -115,8 +98,9 @@ class HourlyIncome(object):
 
     def speed(self):
         """
-        Get the earning speed in Cr/hr
-        :return:
+        Calculate the current credits/hour.
+
+        :return: Calculated credits/hour
         """
         earned = self.trip_earnings()
         if len(self.transactions) > 1:
@@ -127,58 +111,49 @@ class HourlyIncome(object):
             return 0.0
 
     def update_window(self):
-        """
-        Update the EDMC window
-        :return:
-        """
+        """Update our EDMC UI elements."""
         self.update_earned()
         self.update_transaction_rate()
         self.update_hourlyincome()
 
     def update_transaction_rate(self):
         """
-        Set the transaction rate rate in the EDMC window
+        Set the transaction rate rate in the EDMC window.
+
         :param msg:
-        :return:
         """
-        msg = "{} Visits/hr".format(Locale.stringFromNumber(self.rate(), 2))
+        msg = f"{Locale.stringFromNumber(self.rate(), 2)} Visits/hr"
         self.rate_widget.after(0, self.rate_widget.config, {"text": msg})
 
     def update_hourlyincome(self):
         """
-        Set the transaction speed rate in the EDMC window
+        Set the transaction speed rate in the EDMC window.
+
         :param msg:
-        :return:
         """
-        msg = "{} Cr/hr".format(Locale.stringFromNumber(self.speed(), 2))
+        msg = f"{Locale.stringFromNumber(self.speed(), 2)} Cr/hr"
         self.speed_widget.after(0, self.speed_widget.config, {"text": msg})
 
     def update_earned(self):
         """
-        Set the transaction speed rate in the EDMC window
+        Set the transaction speed rate in the EDMC window.
+
         :param msg:
-        :return:
         """
-        msg = "{} Cr".format(Locale.stringFromNumber(self.trip_earnings() + self.saved_earnings, 2))
+        msg = f"{Locale.stringFromNumber(self.trip_earnings() + self.saved_earnings, 2)} Cr"
         self.earned_widget.after(0, self.earned_widget.config, {"text": msg})
 
 
-def plugin_start():
+def plugin_start3(plugin_dir):
+    """Plugin start-up."""
     hourlyincome = HourlyIncome()
     hourlyincome.load()
     this.hourlyincome = hourlyincome
     # this.hourlyincome.transaction(0)
 
-def plugin_start3(plugin_dir):
-    hourlyincome = HourlyIncome()
-    hourlyincome.load()
-    this.hourlyincome = hourlyincome
-    # this.hourlyincome.transaction(0)
 
 def plugin_app(parent):
-    """
-    Create a pair of TK widgets for the EDMC main window
-    """
+    """Create a pair of TK widgets for the EDMC main window."""
     hourlyincome = this.hourlyincome
 
     frame = tk.Frame(parent)
@@ -217,9 +192,10 @@ def plugin_app(parent):
     return frame
 
 
-def journal_entry(cmdr, is_beta, system, station, entry, state):
+def journal_entry(cmdr, is_beta, system, station, entry, state):  # noqa: CCR001, C901
     """
-    Process a journal event
+    Process a journal event.
+
     :param cmdr:
     :param system:
     :param station:
@@ -228,74 +204,119 @@ def journal_entry(cmdr, is_beta, system, station, entry, state):
     :return:
     """
     if "event" in entry:
+        #######################################################################
         # ! trading
+        #######################################################################
         if "MarketSell" in entry["event"]:
             this.hourlyincome.transaction(entry["TotalSale"])
+
         elif "MarketBuy" in entry["event"]:
             this.hourlyincome.transaction(-entry["TotalCost"])
+
         elif "BuyTradeData" in entry["event"]:
             this.hourlyincome.transaction(-entry["Cost"])
+        #######################################################################
+
+        #######################################################################
         # ! refuel/repair/restock
+        #######################################################################
         elif "BuyAmmo" in entry["event"]:
             this.hourlyincome.transaction(-entry["Cost"])
+
         elif "BuyDrones" in entry["event"]:
             this.hourlyincome.transaction(-entry["TotalCost"])
+
         elif "SellDrones" in entry["event"]:
             this.hourlyincome.transaction(entry["TotalSale"])
+
         elif "RefuelAll" in entry["event"]:
             this.hourlyincome.transaction(-entry["Cost"])
+
         elif "RefuelPartial" in entry["event"]:
             this.hourlyincome.transaction(-entry["Cost"])
+
         elif "Repair" in entry["event"]:
             this.hourlyincome.transaction(-entry["Cost"])
+
         elif "RepairAll" in entry["event"]:
             this.hourlyincome.transaction(-entry["Cost"])
+
         elif "RestockVehicle" in entry["event"]:
             this.hourlyincome.transaction(-entry["Cost"])
+        #######################################################################
+
+        #######################################################################
         # ! shipyard/outfitting/engineering
+        #######################################################################
         elif "ModuleBuy" in entry["event"]:
             this.hourlyincome.transaction(-entry["Buyprice"])
             if "SellItem" in entry:
                 this.hourlyincome.transaction(entry["SellPrice"])
+
         elif "ModuleSell" in entry["event"]:
             this.hourlyincome.transaction(entry["SellPrice"])
+
         elif "ModuleSellRemote" in entry["event"]:
             this.hourlyincome.transaction(entry["SellPrice"])
+
         elif "FetchRemoteModule" in entry["event"]:
             this.hourlyincome.transaction(-entry["TransferCost"])
+
         elif "ShipyardBuy" in entry["event"]:
             this.hourlyincome.transaction(-entry["ShipPrice"])
             if "SellOldShip" in entry:
                 this.hourlyincome.transaction(entry["SellPrice"])
+
         elif "ShipyardSell" in entry["event"]:
             this.hourlyincome.transaction(entry["ShipPrice"])
+
         elif "ShipyardTransfer" in entry["event"]:
             this.hourlyincome.transaction(-entry["TransferPrice"])
+
         elif "EngineerContribution" in entry["event"] and "Credits" in entry["Type"]:
             this.hourlyincome.transaction(-entry["Quantity"])
+        #######################################################################
+
+        #######################################################################
         # ! fees
+        #######################################################################
         elif "PayBounties" in entry["event"]:
             this.hourlyincome.transaction(-entry["Amount"])
+
         elif "PayFines" in entry["event"]:
             this.hourlyincome.transaction(-entry["Amount"])
+
         elif "PayLegacyFines" in entry["event"]:
             this.hourlyincome.transaction(-entry["Amount"])
+        #######################################################################
+
+        #######################################################################
         # ! combat
+        #######################################################################
         elif "RedeemVoucher" in entry["event"]:
             this.hourlyincome.transaction(entry["Amount"])
-        # ? These are probably logged upon award of bond/voucher and not upon payment; Instead, RedeemVoucher is logged upon payment
+        # ? These are probably logged upon award of bond/voucher and not upon
+        # payment; Instead, RedeemVoucher is logged upon payment
         # elif "Bounty" in entry["event"]:
         #     this.hourlyincome.transaction(entry["TotalReward"])
         # elif "CapShipBond" in entry["event"]:
         #     this.hourlyincome.transaction(entry["Reward"])
         # elif "FactionKillBond" in entry["event"]:
         #     this.hourlyincome.transaction(entry["Reward"])
+        #######################################################################
+
+        #######################################################################
         # ! exploration
+        #######################################################################
         elif "BuyExplorationData" in entry["event"]:
             this.hourlyincome.transaction(-entry["Cost"])
         elif "SellExplorationData" in entry["event"]:
             this.hourlyincome.transaction(entry["TotalEarnings"])
+        #######################################################################
+
+        #######################################################################
         # ! missions/community goals/search and rescue
+        #######################################################################
         elif "CommunityGoalReward" in entry["event"]:
             this.hourlyincome.transaction(entry["Reward"])
         elif "SearchAndRescue" in entry["event"]:
@@ -305,21 +326,38 @@ def journal_entry(cmdr, is_beta, system, station, entry, state):
                 this.hourlyincome.transaction(-entry["Dontation"])
             else:
                 this.hourlyincome.transaction(entry["Reward"])
+        #######################################################################
+
+        #######################################################################
         # ! npc crew
+        #######################################################################
         elif "CrewHire" in entry["event"]:
             this.hourlyincome.transaction(-entry["Cost"])
         elif "NpcCrewPaidWage" in entry["event"]:
             this.hourlyincome.transaction(-entry["Amount"])
+        #######################################################################
+
+        #######################################################################
         # ! rebuy
+        #######################################################################
         elif "SellShipOnRebuy" in entry["event"]:
             this.hourlyincome.transaction(entry["ShipPrice"])
         elif "Resurrect" in entry["event"]:
             this.hourlyincome.transaction(-entry["Cost"])
+        #######################################################################
+
+        #######################################################################
         # ! powerplay
+        #######################################################################
         elif "PowerplayFastTrack" in entry["event"]:
             this.hourlyincome.transaction(-entry["cost"])
         elif "PowerplaySalary" in entry["event"]:
             this.hourlyincome.transaction(entry["Amount"])
+        #######################################################################
+
+        #######################################################################
         # ! is_docking_event
+        #######################################################################
         elif "Docked" in entry["event"]:
             this.hourlyincome.register_docking()
+        #######################################################################
